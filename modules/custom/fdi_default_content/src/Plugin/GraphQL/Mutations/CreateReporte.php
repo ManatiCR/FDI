a75c434fdd8d25d4920a1c93ff2c94db796c8751
Josue2591 <josuegv2591@gmail.com>
2018-03-01 17:16:06 -0600

    FDI-33: Generate a unique code when the reporte is created.

--- modules/custom/fdi_default_content/src/Plugin/GraphQL/Mutations/CreateReporte.php
+++ modules/custom/fdi_default_content/src/Plugin/GraphQL/Mutations/CreateReporte.php
@@ -30,11 +30,21 @@ class CreateReporte extends CreateEntityBase {
    * {@inheritdoc}
    */
   protected function extractEntityInput(array $input_args, InputObjectType $input_type, ResolveInfo $info) {
+    $codigo_seguimiento = 0;
+    $result = 1;
+    do {
+      $codigo_seguimiento = substr(md5(uniqid(mt_rand(), true)), 0, 8);
+      $query = \Drupal::service('entity.query')
+        ->get('node')
+        ->condition('type', 'reporte')
+        ->condition('field_codigo_de_seguimient', $codigo_seguimiento);
+      $result = $query->execute();
+    } while (count($result) > 0);
     return [
       'title' => $input_args['title'],
       'field_correo' => $input_args['field_correo'],
       'body' => $input_args['body'],
-      'field_codigo_de_seguimient' => $input_args['field_codigo_de_seguimient'],
+      'field_codigo_de_seguimient' => $codigo_seguimiento,
       'field_categoria_reporte' => $input_args['field_categoria_reporte'],
       'field_subcategoria_reporte' => $input_args['field_subcategoria_reporte'],
       'field_latitud' => $input_args['field_latitud'],
